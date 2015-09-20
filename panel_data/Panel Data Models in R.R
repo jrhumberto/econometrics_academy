a6af9fba9efdf3aab0a0695e4ec15570b0831ffc
sdevine188 <sdevine188@gmail.com>
2015-09-20 19:54:32 -0400

    updated file

--- panel_data/Panel Data Models in R.R
+++ panel_data/Panel Data Models in R.R
@@ -21,12 +21,72 @@ summary(X)
 # optional to remove intercept
 # same coefficients and standard errors as plm, but takes longer to calculate and has messy output
 # also, oddly does not remove time invariant education variable??
+# without intercept
 d_fixed <- lm(Y ~ X + factor(id) - 1, data = pdata)
 summary(d_fixed)
+d_fixed$coefficients[1:4]
+
+# with intercept
+# different standard errors than without intercept
+# interesting that main coefficients are the same, except the time invariant education
+d_fixed1 <- lm(Y ~ X + factor(id), data = pdata)
+summary(d_fixed)
+d_fixed1$coefficients[2:5]
+
+# test spelling out the equation
+# same as pre-staged X and Y argument version, with and without intercept
+d_fixed3 <- lm(lwage ~ exp + exp2 + wks + ed + factor(id) - 1, data = pdata)
+summary(d_fixed)
+d_fixed3$coefficients[1:4]
+
+d_fixed4 <- lm(lwage ~ exp + exp2 + wks + ed + factor(id), data = pdata)
+summary(d_fixed)
+d_fixed4$coefficients[2:5]
+
+# test of fixed effects with least squares dummy variable model
+# d_fixed 5 and 6 both have same coefficients and standard errors as each other
+# see if i get the same coefficients when taking out education, which is time invariant and dropped from fixed effects model
+# it is the same coefficients as fixed effects model and same standard error
+d_fixed5 <- lm(lwage ~ exp + exp2 + wks + factor(id) - 1, data = pdata)
+summary(d_fixed5)
+d_fixed5$coefficients[1:4]
+
+d_fixed6 <- lm(lwage ~ exp + exp2 + wks + factor(id), data = pdata)
+summary(d_fixed6)
+d_fixed6$coefficients[2:5]
+
+# ls dummy variable model with time dummies
+
 
 # Fixed effects or within estimator using plm
+# plm must automatically use "id" variable as the unit of fixed effects??
 fixed <- plm(Y ~ X, data=pdata, model= "within")
 summary(fixed)
+fixed$coefficients[1:4]
+
+# fixed estimator with variables added manually
+# same coefficients and standard errors as using X and Y grouped variables
+fixed2 <- plm(lwage ~ exp + exp2 + wks, data=pdata, model= "within")
+summary(fixed2)
+fixed2$coefficients[1:4]
+
+# try including education as a variable to see if it gets dropped
+# it does get dropped, though it doesn't tell you that
+fixed3 <- plm(lwage ~ exp + exp2 + wks + ed, data=pdata, model= "within")
+summary(fixed3)
+fixed3$coefficients[1:4]
+
+# one last test to see if it will include a newly made time-varying variable
+# it does include the test, so ed is being properly recognized as time-invariant and thus dropped
+# results are same as when I manually drop ed in lm model
+# key question is how lm model allows me to estimate coefficient for ed?  
+# is it bc ed is not perfectly collinear with any one variable, it doesn't drop it
+# even though using "difference from individual average" method of fixed effects it does end up perfectly collinear
+# bc dummy minus the average for a dummy is zero, so no variation to use
+pdata$test <- pdata$lwage + pdata$ed
+fixed4 <- plm(lwage ~ exp + exp2 + wks + test, data=pdata, model= "within")
+summary(fixed4)
+fixed4$coefficients[1:4]
 
 # fixed effects with time effects using plm
 fixed_time <- plm(Y ~ X + factor(t), data=pdata, model= "within")
